1e28a880a1682ddfbf09af95b1658a64dbac8904
DayaaaanAK <95142739+DayaaaanAK@users.noreply.github.com>
2022-03-22 01:17:56 +0600

    Add files via upload

---
+++ LabTask10/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace LabTask10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Employee_List_ID_Name = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Employee_ID_Input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.PCT_Output = new System.Windows.Forms.Label();
+            this.Manager_Output = new System.Windows.Forms.Label();
+            this.Department_Output = new System.Windows.Forms.Label();
+            this.ID_Output = new System.Windows.Forms.Label();
+            this.Email_Output = new System.Windows.Forms.Label();
+            this.Phone_Output = new System.Windows.Forms.Label();
+            this.Date_Output = new System.Windows.Forms.Label();
+            this.Job_Output = new System.Windows.Forms.Label();
+            this.Name_Output = new System.Windows.Forms.Label();
+            this.Salary_Output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Employee_List_ID_Name
+            // 
+            this.Employee_List_ID_Name.FormattingEnabled = true;
+            this.Employee_List_ID_Name.ItemHeight = 16;
+            this.Employee_List_ID_Name.Location = new System.Drawing.Point(22, 45);
+            this.Employee_List_ID_Name.Name = "Employee_List_ID_Name";
+            this.Employee_List_ID_Name.Size = new System.Drawing.Size(345, 356);
+            this.Employee_List_ID_Name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(491, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // Employee_ID_Input
+            // 
+            this.Employee_ID_Input.Location = new System.Drawing.Point(528, 45);
+            this.Employee_ID_Input.Name = "Employee_ID_Input";
+            this.Employee_ID_Input.Size = new System.Drawing.Size(235, 22);
+            this.Employee_ID_Input.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(491, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Salary:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(491, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(491, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Job ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(491, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Hire Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(491, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Phone Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(491, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Email:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(491, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "ID:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(810, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Search_OnClick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(491, 483);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 20);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Department ID:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(491, 447);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(101, 20);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Manager ID:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(491, 407);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(136, 20);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Commision PCT:";
+            // 
+            // PCT_Output
+            // 
+            this.PCT_Output.AutoSize = true;
+            this.PCT_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PCT_Output.Location = new System.Drawing.Point(633, 407);
+            this.PCT_Output.Name = "PCT_Output";
+            this.PCT_Output.Size = new System.Drawing.Size(0, 20);
+            this.PCT_Output.TabIndex = 24;
+            // 
+            // Manager_Output
+            // 
+            this.Manager_Output.AutoSize = true;
+            this.Manager_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Manager_Output.Location = new System.Drawing.Point(633, 447);
+            this.Manager_Output.Name = "Manager_Output";
+            this.Manager_Output.Size = new System.Drawing.Size(0, 20);
+            this.Manager_Output.TabIndex = 23;
+            // 
+            // Department_Output
+            // 
+            this.Department_Output.AutoSize = true;
+            this.Department_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Department_Output.Location = new System.Drawing.Point(633, 483);
+            this.Department_Output.Name = "Department_Output";
+            this.Department_Output.Size = new System.Drawing.Size(0, 20);
+            this.Department_Output.TabIndex = 22;
+            // 
+            // ID_Output
+            // 
+            this.ID_Output.AutoSize = true;
+            this.ID_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID_Output.Location = new System.Drawing.Point(633, 144);
+            this.ID_Output.Name = "ID_Output";
+            this.ID_Output.Size = new System.Drawing.Size(0, 20);
+            this.ID_Output.TabIndex = 21;
+            // 
+            // Email_Output
+            // 
+            this.Email_Output.AutoSize = true;
+            this.Email_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email_Output.Location = new System.Drawing.Point(633, 190);
+            this.Email_Output.Name = "Email_Output";
+            this.Email_Output.Size = new System.Drawing.Size(0, 20);
+            this.Email_Output.TabIndex = 20;
+            // 
+            // Phone_Output
+            // 
+            this.Phone_Output.AutoSize = true;
+            this.Phone_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Phone_Output.Location = new System.Drawing.Point(633, 238);
+            this.Phone_Output.Name = "Phone_Output";
+            this.Phone_Output.Size = new System.Drawing.Size(0, 20);
+            this.Phone_Output.TabIndex = 19;
+            // 
+            // Date_Output
+            // 
+            this.Date_Output.AutoSize = true;
+            this.Date_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date_Output.Location = new System.Drawing.Point(633, 281);
+            this.Date_Output.Name = "Date_Output";
+            this.Date_Output.Size = new System.Drawing.Size(0, 20);
+            this.Date_Output.TabIndex = 18;
+            // 
+            // Job_Output
+            // 
+            this.Job_Output.AutoSize = true;
+            this.Job_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Job_Output.Location = new System.Drawing.Point(633, 323);
+            this.Job_Output.Name = "Job_Output";
+            this.Job_Output.Size = new System.Drawing.Size(0, 20);
+            this.Job_Output.TabIndex = 17;
+            // 
+            // Name_Output
+            // 
+            this.Name_Output.AutoSize = true;
+            this.Name_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name_Output.Location = new System.Drawing.Point(633, 97);
+            this.Name_Output.Name = "Name_Output";
+            this.Name_Output.Size = new System.Drawing.Size(0, 20);
+            this.Name_Output.TabIndex = 16;
+            // 
+            // Salary_Output
+            // 
+            this.Salary_Output.AutoSize = true;
+            this.Salary_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salary_Output.Location = new System.Drawing.Point(633, 365);
+            this.Salary_Output.Name = "Salary_Output";
+            this.Salary_Output.Size = new System.Drawing.Size(0, 20);
+            this.Salary_Output.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 666);
+            this.Controls.Add(this.PCT_Output);
+            this.Controls.Add(this.Manager_Output);
+            this.Controls.Add(this.Department_Output);
+            this.Controls.Add(this.ID_Output);
+            this.Controls.Add(this.Email_Output);
+            this.Controls.Add(this.Phone_Output);
+            this.Controls.Add(this.Date_Output);
+            this.Controls.Add(this.Job_Output);
+            this.Controls.Add(this.Name_Output);
+            this.Controls.Add(this.Salary_Output);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Employee_ID_Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Employee_List_ID_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Employee_List_ID_Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Employee_ID_Input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label PCT_Output;
+        private System.Windows.Forms.Label Manager_Output;
+        private System.Windows.Forms.Label Department_Output;
+        private System.Windows.Forms.Label ID_Output;
+        private System.Windows.Forms.Label Email_Output;
+        private System.Windows.Forms.Label Phone_Output;
+        private System.Windows.Forms.Label Date_Output;
+        private System.Windows.Forms.Label Job_Output;
+        private System.Windows.Forms.Label Name_Output;
+        private System.Windows.Forms.Label Salary_Output;
+    }
+}
+
---
+++ LabTask10/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTask10
+{
+    public partial class Form1 : Form
+    {
+        List<string> Name_List = new List<string>();
+        List<string> Last_Name_List = new List<string>();
+        List<string> ID_List = new List<string>();
+        List<string> Email_List = new List<string>();
+        List<string> Phone_List = new List<string>();
+        List<string> Date_List = new List<string>();
+        List<string> Commission_List = new List<string>();
+        List<string> Manager_ID_List = new List<string>();
+        List<string> Dept_ID_List = new List<string>();
+        List<string> Job_ID_List = new List<string>();
+        List<string> Salary_List = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            using (var reader = new StreamReader(@"C:\employees.csv"))
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    ID_List.Add(values[0]);
+                    Name_List.Add(values[1]);
+                    Last_Name_List.Add(values[2]);
+                    Email_List.Add(values[3]);
+                    Phone_List.Add(values[4]);
+                    Date_List.Add(values[5]);
+                    Job_ID_List.Add(values[6]);
+                    Salary_List.Add(values[7]);
+                    Commission_List.Add(values[8]);
+                    Manager_ID_List.Add(values[9]);
+                    Dept_ID_List.Add (values[10]);
+                }
+                reader.Close();
+                for(int i = 0; i < Name_List.Count; i++)
+                {
+                    if(i==0)
+                    {
+                        string dummy = ID_List[i] + "\t" + Name_List[i];
+                        Employee_List_ID_Name.Items.Add(dummy);
+                    }
+                    else
+                    {
+                        string dummy = ID_List[i] + "\t" + "\t" + Name_List[i];
+                        Employee_List_ID_Name.Items.Add(dummy);
+                    }
+                }
+            }
+
+        }
+
+        private void Search_OnClick(object sender, EventArgs e)
+        {
+            var id = Employee_ID_Input.Text;
+            int i, flag = 0;
+            string ID = id.ToString();
+            for(i=0; i < ID_List.Count; i++)
+            {
+                if(id == ID_List[i])
+                {
+                    flag = 1;
+                    Name_Output.Text = Name_List[i] +" " + Last_Name_List[i];
+                    ID_Output.Text = ID_List[i];
+                    Email_Output.Text = Email_List[i];
+                    Phone_Output.Text = Phone_List[i];
+                    Date_Output.Text = Date_List[i];
+                    Job_Output.Text = Job_ID_List[i];
+                    Salary_Output.Text = Salary_List[i];
+                    PCT_Output.Text = Commission_List[i];
+                    Manager_Output.Text = Manager_ID_List[i];
+                    Department_Output.Text = Dept_ID_List[i];
+
+                    StreamWriter s = File.AppendText("d:\\log.txt");
+                    s.WriteLine(ID_List[i] + "," + Name_List[i] + " " + Last_Name_List[i] + "," + Email_List[i] + "," + Phone_List[i] + "," + Date_List[i] + "," + Job_ID_List[i] + "," + Salary_List[i] + "," + Commission_List[i] + "," + Manager_ID_List[i] + "," + Dept_ID_List[i]);
+                    s.Close();
+                }
+            }
+            if(flag == 0)
+            {
+                MessageBox.Show("ID does not exist");
+                StreamWriter s = File.AppendText("d:\\log.txt");
+                s.WriteLine(ID + "," + "ID doesn't exist");
+                s.Close();
+            }
+        }
+    }
+
+}
